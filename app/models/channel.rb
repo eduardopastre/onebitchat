e5ac7f95606713be0d06e67abb20eab4cf4e1dc9
Eduardo Pastre <dupastre@gmail.com>
2018-05-16 21:58:23 -0300

    create project models

---
+++ app/models/channel.rb
@@ -0,0 +1,7 @@
+class Channel < ApplicationRecord
+  has_many :messages, as: :messagable, :dependent => :destroy
+  belongs_to :team
+  belongs_to :user
+  
+  validates_presence_of :slug, :team, :user
+end
