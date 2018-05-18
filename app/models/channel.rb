a689724818f42a09ea6683ae093d5370505f33c4
Eduardo Pastre <dupastre@gmail.com>
2018-05-17 22:20:29 -0300

    models adjusts

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,4 +5,5 @@ class Channel < ApplicationRecord
   
   validates_presence_of :slug, :team, :user
   validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates_uniqueness_of :slug, :scope => :team_id
 end
