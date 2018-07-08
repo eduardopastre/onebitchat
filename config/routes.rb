babf0a44c060644a4ec34e6ec78cfe5618c465c3
Eduardo Pastre <dupastre@gmail.com>
2018-07-08 16:30:32 -0300

    crate action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root to: 'teams#index'
-  resources :teams, only: [:create, :destroy]
+
+  mount ActionCable.server => '/cable'
+
   get '/:slug', to: 'teams#show'
+  
+  resources :teams, only: [:create, :destroy]
   resources :channels, only: [:show, :create, :destroy]
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
