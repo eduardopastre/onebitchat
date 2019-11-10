3f978807bcb7c383ac73d4d2e84ef41ae244682c
eduardo pastre <dupastre@gmail.com>
2019-11-10 17:30:25 -0200

    updating gems ruby and rails version

--- db/schema.rb
+++ db/schema.rb
@@ -10,71 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180518004927) do
+ActiveRecord::Schema.define(version: 2018_05_18_004927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "channels", force: :cascade do |t|
-    t.string   "slug"
-    t.integer  "user_id"
-    t.integer  "team_id"
+  create_table "channels", id: :serial, force: :cascade do |t|
+    t.string "slug"
+    t.integer "user_id"
+    t.integer "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["team_id"], name: "index_channels_on_team_id", using: :btree
-    t.index ["user_id"], name: "index_channels_on_user_id", using: :btree
+    t.index ["team_id"], name: "index_channels_on_team_id"
+    t.index ["user_id"], name: "index_channels_on_user_id"
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "user_id"
-    t.integer  "messagable_id"
-    t.string   "messagable_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "talks", force: :cascade do |t|
-    t.integer  "user_one_id"
-    t.integer  "user_two_id"
-    t.integer  "team_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["team_id"], name: "index_talks_on_team_id", using: :btree
-  end
-
-  create_table "team_users", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "team_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["team_id"], name: "index_team_users_on_team_id", using: :btree
-    t.index ["user_id"], name: "index_team_users_on_user_id", using: :btree
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "slug"
-    t.integer  "user_id"
+  create_table "messages", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "messagable_id"
+    t.string "messagable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "talks", id: :serial, force: :cascade do |t|
+    t.integer "user_one_id"
+    t.integer "user_two_id"
+    t.integer "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_talks_on_team_id"
+  end
+
+  create_table "team_users", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_team_users_on_team_id"
+    t.index ["user_id"], name: "index_team_users_on_user_id"
+  end
+
+  create_table "teams", id: :serial, force: :cascade do |t|
+    t.string "slug"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "name",                                null: false
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "channels", "teams"
